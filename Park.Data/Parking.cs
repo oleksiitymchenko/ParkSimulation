56e9875c796e54980234b583f496f7db3afacc11
oleksiitymchenko <36442865+oleksiitymchenko@users.noreply.github.com>
2018-05-10 14:39:54 +0300

    Added fields and ctors to Parking

--- Park.Data/Parking.cs
+++ Park.Data/Parking.cs
@@ -8,6 +8,17 @@ namespace Park.Data
 {
     public class Parking
     {
-        
+        private static readonly Lazy<Parking> _instanse = new Lazy<Parking>(() => new Parking());
+
+        public static Parking Instance
+        {
+            get
+                {return _instanse.Value;}
+        }
+        private Parking(){ }
+
+        private List<Car> _cars = new List<Car>();
+        private List<Transaction> _transactions = new List<Transaction>();
+        private double Balance = 0;
     }
 }
